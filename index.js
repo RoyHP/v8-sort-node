d0b250c22c013ae62af52daa1f8e626d95b50e90
Roy Hill-Percival <github@royhp.com>
2016-10-04 13:36:10 -0400

    Some ES6 cleanup

--- index.js
+++ index.js
@@ -40,42 +40,42 @@ var heapSort = (function () {
     }
 }())
 
-var quickSort = (function () {
+var quickSort = (() => {
 
     function partition(array, left, right) {
         var cmp = array[right - 1],
             minEnd = left,
-            maxEnd;
+            maxEnd
         for (maxEnd = left; maxEnd < right - 1; maxEnd += 1) {
             if (array[maxEnd] <= cmp) {
-                swap(array, maxEnd, minEnd);
-                minEnd += 1;
+                swap(array, maxEnd, minEnd)
+                minEnd += 1
             }
         }
-        swap(array, minEnd, right - 1);
-        return minEnd;
+        swap(array, minEnd, right - 1)
+        return minEnd
     }
 
     function swap(array, i, j) {
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-        return array;
+        var temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+        return array
     }
 
     function quickSort(array, left, right) {
         if (left < right) {
-            var p = partition(array, left, right);
-            quickSort(array, left, p);
-            quickSort(array, p + 1, right);
+            var p = partition(array, left, right)
+            quickSort(array, left, p)
+            quickSort(array, p + 1, right)
         }
-        return array;
+        return array
     }
 
-    return function (array) {
-        return quickSort(array, 0, array.length);
-    };
-}());
+    return array => {
+        return quickSort(array, 0, array.length)
+    }
+}())
 
 function getRandomInt(min, max) {
   min = Math.ceil(min)
